9f98afb5facb921047c1de7c3d495c94b6f3bd4c
DaverMc <pommesesser61@gmail.com>
2022-10-04 12:52:18 +0200

    Added sending

--- src/main/java/de/daver/buun/core/lang/MessageProcessor.java
+++ src/main/java/de/daver/buun/core/lang/MessageProcessor.java
@@ -1,6 +1,9 @@
 package de.daver.buun.core.lang;
 
+import de.daver.buun.core.command.Sender;
 import de.daver.buun.core.util.StringFormatter;
+
+import java.util.List;
 
 public class MessageProcessor extends StringFormatter {
 
@@ -10,6 +13,16 @@ public class MessageProcessor extends StringFormatter {
 
     public MessageProcessor setValue(String name, Object value){
         string = string.replaceAll("<" + name + ">", String.valueOf(value));
+        return this;
+    }
+
+    public MessageProcessor send(Sender sender){
+        sender.sendMessage(get());
+        return this;
+    }
+
+    public MessageProcessor broadcast(List<Sender> senders){
+        senders.forEach(this::send);
         return this;
     }
 
